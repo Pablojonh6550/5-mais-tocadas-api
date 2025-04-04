c90bc54bc4347e9a405becd1a54c8a2c47115d30
Pablojonh6550 <pablojonh6550@gmail.com>
2025-04-04 16:35:22 -0300

    refactor: adição campo de tipo

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Models\Music;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -20,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -43,5 +47,40 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Implementation of the JWTSubject interface method.
+     * Returns the model's primary key identifier.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'role' => $this->role
+        ];
+    }
+
+    /**
+     * Return a relationship for all the music indications made by this user.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<\App\Models\Music>
+     */
+    public function myMusicIndications(): HasMany
+    {
+        return $this->hasMany(Music::class);
     }
 }
